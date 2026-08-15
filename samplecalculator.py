65518cf02689618422e414736f0cc5358c40a9c8
BAUTISTA_PLM_TEST <gaebautista2026@plm.edu.ph>
2026-08-15 08:04:02 +0800

    samplcalculator.py

---
+++ samplecalculator.py
@@ -0,0 +1,17 @@
+print("gooooffy ahh callculator")
+
+input(" +/- = ")
+
+if " + ":
+    print("addition")
+    first = int(input("Enter a number 1: "))
+    second = int(input("Enter a number 2: "))
+    sum = first + second
+    print("The sum of the two numbers is: ", sum)
+else:
+    print("subtraction")
+    third = int(input("Enter a number 3: "))
+    fourth = int(input("Enter a number 4: "))
+    dif = third - fourth
+    print("The difference of the two numbers is: ", dif)
+
