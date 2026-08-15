90821d8b4a01a3128532a903ed048c89f62a6955
unknown <gaebautista2026@plm.edu.ph>
2026-08-15 08:29:11 +0800

    samplecalculator.py

--- samplecalculator.py
+++ samplecalculator.py
@@ -1,17 +1,18 @@
 print("gooooffy ahh callculator")
 
-input(" +/- = ")
+opera = input(" +/- =  ")
 
-if " + ":
+if opera == "+":
     print("addition")
     first = int(input("Enter a number 1: "))
     second = int(input("Enter a number 2: "))
     sum = first + second
     print("The sum of the two numbers is: ", sum)
-else:
+elif opera == "-":
     print("subtraction")
     third = int(input("Enter a number 3: "))
     fourth = int(input("Enter a number 4: "))
     dif = third - fourth
     print("The difference of the two numbers is: ", dif)
-
+else:
+    print("dumdum")
