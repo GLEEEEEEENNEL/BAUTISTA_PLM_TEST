95bf57bc6390f67d3345af2aa0bff4d61022ca79
BAUTISTA_PLM_TEST <gaebautista2026@plm.edu.ph>
2026-08-16 08:58:13 +0800

    Save local changes to sample1.py before pulling

--- sample1.py
+++ sample1.py
@@ -1,1 +1,30 @@
-print("hello world")
+print("gooooffy ahh callculator")
+
+
+#first = int(input("Enter a number 1: "))
+#second = int(input("Enter a number 2: "))
+#sum = first + second
+#print("The sum of the two numbers is: ", sum)
+
+
+#r = int(input("Enter a number: "))
+#Pi = 3.1416
+#Area = Pi * (r ** 2)
+#print("The area of the circle is: ", Area)
+
+
+#Q1 = int(input("Enter a number: "))
+#Q2 = int(input("Enter a number: "))
+#Q3 = int(input("Enter a number: "))
+
+#sum = Q1 + Q2 + Q3 / 3
+#print("The average of the three numbers is: ", sum)
+
+#Fahreinheit = int(input("Enter a temperature in Fahrenheit: "))
+#Celsius = (Fahreinheit - 32) * 5/9
+#print("The temperature in Celsius is: ", Celsius)
+
+third = int(input("Enter a number 3: "))
+fourth = int(input("Enter a number 4: "))
+dif = third - fourth
+print("The difference of the two numbers is: ", dif)
